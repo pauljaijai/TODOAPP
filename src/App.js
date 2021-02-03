d65826a73f9f1d8fc45dc4aa3cace190213bd3d9
pauljaijai <63136413+pauljaijai@users.noreply.github.com>
2021-02-03 19:45:14 +0800

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+  this.state={
+    newItem:'',
+    list:[]
+  }
+  }
+
+  deleteItem(id){
+    const list =[...this.state.list];
+
+    const updatedList = list.filter (item=> item.id != id);
+
+    this.setState({list:updatedList})
+  }
+
+
+  updateInput(key,value){
+    this.setState({
+      [key]: value
+    });
+
+  }
+  addItem(){
+
+    const newItem={
+      id: 1+ Math.random(),
+      value:this.state.newItem.slice()
+
+    };
+
+
+    const list=[...this.state.list];
+
+    list.push(newItem);
+
+      this.setState({
+        list,
+        newItem:""
+      });
+
+
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="title">Create Your ToDoList</div>
+     <br/>
+     <input
+      type='text'
+      className="input"
+      placeholder="What you wanna do?????"
+      value={this.state.newItem}
+      onChange={e => this.updateInput('newItem' , e.target.value)}
+      />
+
+
+    <button
+      onClick = {() => this .addItem()} className="add"> add</button>
+    <br/>
+    <ul>
+      {this.state.list.map(item => {
+        return(
+          <li className='item' key={item.id}>
+            {item.value}
+            
+            <button
+              className='delete' onClick={() => this.deleteItem(item.id)}>
+
+              Remove
+
+              </button>
+              </li>
+        )
+      })}
+    </ul>
     </div>
   );
 }
-
+}
 export default App;
